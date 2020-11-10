8f71645d84dfc6619577e3edfe608ce87e77b677
shrimp <1732341@qq.com>
2020-11-10 14:25:10 +0800

    商品列表增加名称搜索

--- service/product.go
+++ service/product.go
@@ -25,7 +25,7 @@ func (m *Product) Index() ([]*product.ProductList, error) {
 	tagId, _ := strconv.ParseUint(m.DefaultQuery("tag_id", "0"), 10, 64)
 	page, _ := strconv.ParseUint(m.DefaultQuery("page", "1"), 10, 64)
 	pageSize, _ := strconv.ParseUint(m.DefaultQuery("page_size", "20"), 10, 64)
-	name := m.DefaultPostForm("name", "")
+	name := m.DefaultQuery("name", "")
 	req := &productpb.ListProductReq{
 		CategoryId: categoryId,
 		TagId:      tagId,
